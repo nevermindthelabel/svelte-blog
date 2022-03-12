70ea02433d2091e66ef985f2aa8807d430d26094
Matt <nevermindthelabel@gmail.com>
2022-03-12 09:11:06 -0700

    added mdsvex to project

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
-import sveltePreprocess from 'svelte-preprocess'
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import sveltePreprocess from "svelte-preprocess";
 
 export default {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: [sveltePreprocess(), mdsvex(mdsvexConfig)],
+};
