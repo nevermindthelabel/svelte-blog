4ff6388fb47f58bb28f568d7c37e98dd6088e638
Matt <nevermindthelabel@gmail.com>
2022-03-12 23:55:44 -0700

    initial nav

---
+++ src/components/Nav.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let toggleTheme;
+  export let currentTheme;
+  import Icons from '../assets/svg/icons.svelte';
+  $: console.log(currentTheme);
+</script>
+
+<div class="wrapper">
+  <div class={currentTheme}>routes go here</div>
+  <button class="theme-toggle" on:click={toggleTheme}
+    >{#if currentTheme === 'light'}
+      <Icons theClass="dark-mode-btn" name="darkMode" width="20px" height="20px" fill="#000" />
+    {:else}
+      <Icons theClass="light-mode-btn" name="lightMode" width="20px" height="20px" fill="#fff" />
+    {/if}
+  </button>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    padding: 15px;
+  }
+  .theme-toggle {
+    text-decoration: none;
+    margin-left: auto;
+  }
+  .theme-toggle:focus,
+  .theme-toggle:hover {
+    background-color: orange;
+  }
+  .light {
+    background: #fff;
+    color: #2b2e31;
+    transition: var(--transition);
+  }
+  .dark {
+    background: #2b2e31;
+    color: #fff;
+    transition: var(--transition);
+  }
+</style>
