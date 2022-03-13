880a4788fb5abc79563a667678f6345a320b49d8
Matt <nevermindthelabel@gmail.com>
2022-03-13 16:43:25 -0700

    added dad joke wave

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,7 +3,7 @@
 
 <div class="container">
   <div class="intro">
-    <h1>Hi 👋</h1>
+    <h1 class="greeting">Hi <span>👋</span></h1>
     <p>I'm Matt, a web developer from Phoenix, Az.</p>
     <p>I am passionate about my Faith, Family, green grass, web development, mechanical keyboards, and VS Code.</p>
   </div>
@@ -11,10 +11,48 @@
 
 <style>
   p {
-    /* font-family: 'Victor Mono', monospace; */
     line-height: 150%;
     font-size: 1.25em;
-    /* font-style: italic; */
-    /* font-weight: 600; */
+  }
+  .intro {
+    padding: 2em 1em;
+  }
+  .greeting {
+    font-family: 'Victor Mono', monospace;
+  }
+
+  span:hover {
+    animation-name: howdy;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    transform-origin: 60% 60%;
+    display: inline-block;
+  }
+
+  @keyframes howdy {
+    0% {
+      transform: rotate(0deg);
+    }
+    10% {
+      transform: rotate(14deg);
+    }
+    20% {
+      transform: rotate(-8deg);
+    }
+    30% {
+      transform: rotate(14deg);
+    }
+    40% {
+      transform: rotate(-4deg);
+    }
+    50% {
+      transform: rotate(10deg);
+    }
+    60% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
   }
 </style>
