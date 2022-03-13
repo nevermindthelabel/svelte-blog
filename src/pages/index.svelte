c66d8f098a82faf39986afbea0f857700b5ed709
Matt <nevermindthelabel@gmail.com>
2022-03-13 16:32:07 -0700

    updated text

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,8 +3,18 @@
 
 <div class="container">
   <div class="intro">
-    <h1>Hi</h1>
+    <h1>Hi 👋</h1>
     <p>I'm Matt, a web developer from Phoenix, Az.</p>
-    <p>I am passionate about green grass, technology, web development, mechanical keyboards, and VS Code.</p>
+    <p>I am passionate about my Faith, Family, green grass, web development, mechanical keyboards, and VS Code.</p>
   </div>
 </div>
+
+<style>
+  p {
+    /* font-family: 'Victor Mono', monospace; */
+    line-height: 150%;
+    font-size: 1.25em;
+    /* font-style: italic; */
+    /* font-weight: 600; */
+  }
+</style>
