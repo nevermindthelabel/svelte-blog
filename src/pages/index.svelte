2a54a8edc1c86cb71ffe06a83a17f5dbcba2bf26
Matt <nevermindthelabel@gmail.com>
2022-03-13 00:32:10 -0700

    added home page

---
+++ src/pages/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+  <div class="intro">
+    <h1>Hi</h1>
+    <p>I'm Matt, a web developer from Phoenix, Az.</p>
+    <p>I am passionate about green grass, technology, web development, mechanical keyboards, and VS Code.</p>
+  </div>
+</div>
