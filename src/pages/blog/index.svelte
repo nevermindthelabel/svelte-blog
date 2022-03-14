9bb444a37101e70fc64da45235fa4cc193cbc333
Matt <nevermindthelabel@gmail.com>
2022-03-13 19:56:11 -0700

    wip working now

--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -1,4 +1,39 @@
-<p>blog</p>
+<script context="module" lang="ts">
+  const allPosts = import.meta.glob('./*.{md,svx}');
+  let postData = [];
+  for (let path in allPosts) {
+    postData.push(
+      allPosts[path]().then(({ metadata }) => {
+        return { path, metadata };
+      })
+    );
+  }
+  export const load = async () => {
+    const posts = await Promise.all(postData);
+    return {
+      posts
+      // props: {
+      //   posts
+      // }
+    };
+  };
+</script>
+
+<script>
+  import { onMount } from 'svelte';
+  export let posts;
+  let test;
+  onMount(async () => {
+    posts = await load();
+  });
+  console.log(posts);
+  console.log(test);
+</script>
+
+<div>
+  <!-- <p>blog</p> -->
+  {JSON.stringify(posts)}
+</div>
 
 <style>
   p {
